dddd2e74e8676ffe3da3d00347f37e3cc1027916
Prince Rajput <drprincerajput@gmail.com>
2020-01-26 21:10:16 +0530

    init2

---
+++ guestbook.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+
+	"fmt"
+	"html/template"
+
+	"log"
+	"net/http"
+	"os"
+)
+
+type GuestBook struct {
+	SignatureCount int
+	Signatures     []string
+}
+
+func check(err error)  {
+	if err != nil{
+		log.Fatal(err)
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request){
+	signatures := getStrings("signatures.txt")
+	html, err := template.ParseFiles("view.html")
+	check(err)
+	guestbook := GuestBook{
+		SignatureCount: len(signatures),
+		Signatures: signatures,
+	}
+	err = html.Execute(w, guestbook)
+	check(err)
+}
+
+func newHandler(w http.ResponseWriter, r *http.Request){
+	html, err := template.ParseFiles("new.html")
+	check(err)
+	err = html.Execute(w, nil)
+	check(err)
+}
+
+func createHandler(w http.ResponseWriter, r *http.Request){
+	signature := r.FormValue("signature")
+	options := os.O_WRONLY | os.O_APPEND |os.O_CREATE
+	file, err := os.OpenFile("signatures.txt", options, os.FileMode(0600))
+	check(err)
+	_, err = fmt.Fprintf(file, signature)
+	check(err)
+	err = file.Close()
+	check(err)
+	http.Redirect(w, r, "/guestbook", http.StatusFound)
+
+}
+
+
+
+func getStrings(fileName string) []string  {
+	var lines []string
+	file, err := os.Open(fileName)
+	if os.IsNotExist(err){
+		return nil
+	}
+
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan(){
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
+
+
+
+func main()  {
+	http.HandleFunc("/guestbook", viewHandler)
+	http.HandleFunc("/guestbook/new", newHandler)
+	http.HandleFunc("/guestbook/create", createHandler)
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
+}
